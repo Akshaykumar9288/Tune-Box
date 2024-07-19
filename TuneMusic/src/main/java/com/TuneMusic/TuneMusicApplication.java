3d165cd6bf82bc1bb8a0058c6f54b327da78c359
Akshay kumar <akshayk0552@gmail.com>
2024-07-19 11:35:32 +0530

    Tune Music

---
+++ TuneMusic/src/main/java/com/TuneMusic/TuneMusicApplication.java
@@ -0,0 +1,13 @@
+package com.TuneMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuneMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuneMusicApplication.class, args);
+	}
+
+}
---
+++ TuneMusic/src/test/java/com/TuneMusic/TuneMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TuneMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuneMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
